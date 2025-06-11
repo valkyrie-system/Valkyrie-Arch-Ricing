390870c0895d0ac06f82d29f5ee21af0fadf41c9
Valkyrie System <36176120+valkyrie-system@users.noreply.github.com>
2025-06-10 19:00:18 -0500

    Add files via upload

---
+++ KDE Themes/Expose/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,66 @@
+var panel = new Panel
+var panelScreen = panel.screen
+
+panel.location = "bottom";
+panel.height = 38
+panel.alignment = "center";
+
+var kickoff = panel.addWidget("org.kde.plasma.kickoff")
+kickoff.currentConfigGroup = ["Shortcuts"]
+kickoff.writeConfig("global", "Alt+F1")
+kickoff.currentConfigGroup = ["General"]
+// kickoff.writeConfig("favorites", ["preferred://browser", "org.kde.dolphin.desktop", "org.kde.konsole.desktop", "systemsettings.desktop"])
+panel.addWidget("org.kde.plasma.showdesktop")
+//panel.addWidget("org.kde.plasma.showActivityManager")
+let taskBar = panel.addWidget("org.kde.plasma.taskmanager")
+taskBar.currentConfigGroup = ["General"]
+taskBar.writeConfig("launchers",["preferred://browser", "preferred://filemanager","applications:org.kde.konsole.desktop"])
+panel.addWidget("org.kde.plasma.marginsseparator")
+panel.addWidget("org.kde.plasma.pager")
+
+/* Next up is determining whether to add the Input Method Panel
+ * widget to the panel or not. This is done based on whether
+ * the system locale's language id is a member of the following
+ * white list of languages which are known to pull in one of
+ * our supported IME backends when chosen during installation
+ * of common distributions. */
+
+var langIds = ["as",    // Assamese
+               "bn",    // Bengali
+               "bo",    // Tibetan
+               "brx",   // Bodo
+               "doi",   // Dogri
+               "gu",    // Gujarati
+               "hi",    // Hindi
+               "ja",    // Japanese
+               "kn",    // Kannada
+               "ko",    // Korean
+               "kok",   // Konkani
+               "ks",    // Kashmiri
+               "lep",   // Lepcha
+               "mai",   // Maithili
+               "ml",    // Malayalam
+               "mni",   // Manipuri
+               "mr",    // Marathi
+               "ne",    // Nepali
+               "or",    // Odia
+               "pa",    // Punjabi
+               "sa",    // Sanskrit
+               "sat",   // Santali
+               "sd",    // Sindhi
+               "si",    // Sinhala
+               "ta",    // Tamil
+               "te",    // Telugu
+               "th",    // Thai
+               "ur",    // Urdu
+               "vi",    // Vietnamese
+               "zh_CN", // Simplified Chinese
+               "zh_TW"] // Traditional Chinese
+
+if (langIds.indexOf(languageId) != -1) {
+    panel.addWidget("org.kde.plasma.kimpanel");
+}
+
+panel.addWidget("org.kde.plasma.systemtray")
+panel.addWidget("org.kde.plasma.digitalclock")
+panel.addWidget("org.kde.plasma.lock_logout")
---
+++ KDE Themes/Expose/contents/splash/Splash.qml
@@ -0,0 +1,77 @@
+/*
+ *   Copyright 2014 Marco Martin <mart@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import QtQuick 2.5
+import QtQuick.Window 2.2
+import org.kde.plasma.core 2.0 as PlasmaCore
+import QtGraphicalEffects 1.0
+
+Image {
+    id: root
+    source: "images/Expose.png"
+    fillMode: Image.PreserveAspectCrop
+    property int stage
+
+    onStageChanged: {
+        if (stage == 2) {
+            introAnimation.running = true;
+        } else if (stage == 5) {
+            introAnimation.target = busyIndicator;
+            introAnimation.from = 1;
+            introAnimation.to = 0;
+            introAnimation.running = true;
+        }
+    }
+
+    Item {
+        id: content
+        anchors.fill: parent
+        opacity: 0
+        TextMetrics {
+            id: units
+            text: "M"
+            property int gridUnit: boundingRect.height
+            property int largeSpacing: PlasmaCore.Units.gridUnit
+            property int smallSpacing: Math.max(2, gridUnit/4)
+        }
+
+        Image {
+            id: busyIndicator
+            anchors.centerIn: parent
+            source: "images/busywidget.svg"
+            RotationAnimator on rotation {
+                id: rotationAnimator
+                from: 0
+                to: 360
+                duration: 2400
+                loops: Animation.Infinite
+            }
+        }
+    }
+
+    OpacityAnimator {
+        id: introAnimation
+        running: false
+        target: content
+        from: 0
+        to: 1
+        duration: 1000
+        easing.type: Easing.InOutQuad
+    }
+}
